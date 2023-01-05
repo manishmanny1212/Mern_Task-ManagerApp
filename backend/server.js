61d4d1999a6c5b0940312db685cec6317ecaf37d
manishkr <manishmanny1212@gmail.com>
2023-01-05 19:00:33 +0530

    server created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express=require("express");
 const app= express()
+app.get("/",(req,response)=>{
+    response.send("welcome to home page ")
+})
+app.get("/about",(req,response)=>{
+    response.send("welcome to About page ")
+})
 const PORT= process.env.PORT ||"5000";
 
 app.listen("5000", ()=>{
     console.log(`your server is stared in ${PORT} `)
 })
+
+
+// mongodb+srv://manish1212:<password>@manishcluster.sapuazz.mongodb.net/?retryWrites=true&w=majority
